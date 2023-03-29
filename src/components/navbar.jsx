9eb3ab93705d57a43a4b9ec30d0da0188fbe9e77
VincentChisaka <vincentchisaka2@gmail.com>
2023-03-29 09:18:40 +0300

    Navbar links

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 function Navbar(props) {
     return (
-        <div>
-            <h2>Hello</h2>
-        </div>
+        <nav className="nav">
+            <a href="#" className="brand">herdoy</a>
+            <ul className="nav__menu">
+              <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
+              <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
+              <li className="nav__item"><a href="#" className="nav__link">Admin</a></li>
+              <li className="nav__item"><a href="#" className="nav__link">Contact</a></li>
+              <li className="nav__item"><a href="#" className="nav__link">About</a></li>
+            </ul>
+        </nav>
     );
 }
 
