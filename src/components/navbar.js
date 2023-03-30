7eb8cc2934f16b2df7452e6f79c6d835727d963a
VincentChisaka <vincentchisaka2@gmail.com>
2023-03-30 09:29:58 +0300

    menu bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
+import "/home/vincent/Projects/Group-hotel-app/src/components/navbar.css"
 
 function Navbar() {
+      const [active, setActive] = useState("nav__menu");
+        const navToggle = () => {
+          active === 'nav__menu' ? ''
+        }
+
+        
     return (
         <nav className="nav">
             <a href="#" className="nav__brand">
                 SUMMER HOTELS
                 </a>
-            <ul className="nav__menu">
+            <ul className={active}>
               <li className="nav__item">
-                <a href="#" className="nav__link">Search
+                <a href="#" className="nav__link">
+                  Search
                 </a>
                 </li>
               <li className="nav__item">
-                <a href="#" className="nav__link">Home
+                <a href="#" className="nav__link">
+                  Home
                 </a>
                 </li>
-              <li className="nav__item"><a href="#" className="nav__link">Admin</a></li>
-              <li className="nav__item"><a href="#" className="nav__link">Contact</a></li>
-              <li className="nav__item"><a href="#" className="nav__link">About</a></li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  Admin
+                  </a>
+                </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  Contact
+                  </a>
+                </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  About
+                  </a>
+                  </li>
             </ul>
             <div className="nav__toggler">
                 <div className="line1"></div>
